ada6ed9b8c39cf4c8d6d98d6812396dcde6264b3
Max <maximilian.kruse@kit.edu>
2024-01-27 15:42:25 +0100

    refactor(umbridge posterior): Restructure umbridge posterior

--- models/posterior_umbridge.py
+++ models/posterior_umbridge.py
@@ -1,17 +1,11 @@
-import umbridge
-import numpy as np
-from scipy.stats import multivariate_normal
 import time
-import os
 
-# Inspired by https://github.com/chi-feng/mcmc-demo
+import umbridge
+from scipy.stats import multivariate_normal
 
-class Banana(umbridge.Model):
-    radius = 2.6
-    sigma2 = 0.033
 
-    def __init__(self):
-        super().__init__("posterior")
+class BananaBase(umbridge.Model):
+    _sleep_time = None
 
     def get_input_sizes(self, config):
         return [2]
@@ -19,77 +13,48 @@ class Banana(umbridge.Model):
     def get_output_sizes(self, config):
         return [1]
 
-    def __call__(self, parameters, config = {}):
-        # Random sleep time multiplied by SLEEP_INTERVAL
-        #time.sleep(float(os.environ.get('SLEEP_INTERVAL', .1)) * np.random.random())
-        time.sleep(1)
-
-        a = config.get('a', 2.0)
-        b = config.get('b', 0.2)
-        scale = config.get('scale', 1.0)
-
-        y = [(parameters[0][0] / a),
-             (parameters[0][1] * a + a * b * (parameters[0][0]**2 + a**2))]
-
-        return [[multivariate_normal.logpdf(y, [0, 4], [[1.0*scale, 0.5*scale], [0.5*scale, 1.0*scale]])]]
-
     def supports_evaluate(self):
         return True
+
+    def __call__(self, parameters, config={}):
+        time.sleep(self._sleep_time)
+
+        a = config.get("a", 2.0)
+        b = config.get("b", 0.2)
+        scale = config.get("scale", 1.0)
+        y = [
+            (parameters[0][0] / a),
+            (parameters[0][1] * a + a * b * (parameters[0][0] ** 2 + a**2)),
+        ]
+
+        return [
+            [
+                multivariate_normal.logpdf(
+                    y, [0, 4], [[1.0 * scale, 0.5 * scale], [0.5 * scale, 1.0 * scale]]
+                )
+            ]
+        ]
+
+
+class BananaFine(umbridge.Model):
+    def __init__(self):
+        self._sleep_time = 1
+        super().__init__("posterior_fine")
+
 
 class BananaIntermediate(umbridge.Model):
-    radius = 2.6
-    sigma2 = 0.033
-
     def __init__(self):
+        self._sleep_time = 0.6
         super().__init__("posterior_intermediate")
 
-    def get_input_sizes(self, config):
-        return [2]
-
-    def get_output_sizes(self, config):
-        return [1]
-
-    def __call__(self, parameters, config = {}):
-        time.sleep(.6)
-
-        a = config.get('a', 2.0)
-        b = config.get('b', 0.2)
-        scale = config.get('scale', 1.2)
-
-        y = [(parameters[0][0] / a),
-             (parameters[0][1] * a + a * b * (parameters[0][0]**2 + a**2))]
-
-        return [[multivariate_normal.logpdf(y, [0, 4], [[1.0*scale, 0.5*scale], [0.5*scale, 1.0*scale]])]]
-
-    def supports_evaluate(self):
-        return True
 
 class BananaCoarse(umbridge.Model):
-    radius = 2.6
-    sigma2 = 0.033
-
     def __init__(self):
+        self._sleep_time = 0.3
         super().__init__("posterior_coarse")
 
-    def get_input_sizes(self, config):
-        return [2]
 
-    def get_output_sizes(self, config):
-        return [1]
-
-    def __call__(self, parameters, config = {}):
-        time.sleep(.3)
-
-        a = config.get('a', 2.0)
-        b = config.get('b', 0.2)
-        scale = config.get('scale', 1.5)
-
-        y = [(parameters[0][0] / a),
-             (parameters[0][1] * a + a * b * (parameters[0][0]**2 + a**2))]
-
-        return [[multivariate_normal.logpdf(y, [0, 4], [[1.0*scale, 0.5*scale], [0.5*scale, 1.0*scale]])]]
-
-    def supports_evaluate(self):
-        return True
-
-umbridge.serve_models([Banana(), BananaIntermediate(), BananaCoarse()], port=4243,max_workers=100)
+if __name__ == "__main__":
+    umbridge.serve_models(
+        [BananaFine(), BananaIntermediate(), BananaCoarse()], port=4243, max_workers=100
+    )

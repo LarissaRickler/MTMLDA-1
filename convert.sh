2884d0d79b28c7cacabb0e5a741f3277239c2702
Max <maximilian.kruse@kit.edu>
2024-01-27 12:56:36 +0100

    chore(visualization): Disable video creation in shell script

--- convert.sh
+++ convert.sh
@@ -5,4 +5,4 @@ do
   dot -Tpng $file -o $file.png
 done
 
-ffmpeg -framerate 5 -i mtmc%05d.dot.png -vf scale=1280:720 -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
+#ffmpeg -framerate 5 -i mtmc%05d.dot.png -vf scale=1280:720 -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4

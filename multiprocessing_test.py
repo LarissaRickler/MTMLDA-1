2354fb638e3e27abd4724fa2a1d40674abdafba3
Max <maximilian.kruse@kit.edu>
2024-01-27 12:58:01 +0100

    feat(multiprocessing): Add draft for multi-chain run driver

---
+++ multiprocessing_test.py
@@ -0,0 +1,61 @@
+import functools
+import multiprocessing
+import os
+import pathlib
+
+import numpy as np
+
+# --------------------------------------------------------------------------------------------------
+num_processes = 2
+
+class Settings:
+    num_parameters = 4
+    parameter_ranges = [[500, 5000], [1e5, 1e6], [5, 50], [5, 50]]
+    output_directory = pathlib.Path("results")
+    output_filename = "chain"
+    overwrite_results = True
+
+
+# --------------------------------------------------------------------------------------------------
+def execute_mtmlda_run(process_id, settings):
+    initial_state = _get_initial_state(process_id, settings)
+    mcmc_trace = _run_chain(process_id, initial_state, settings)
+    _save_trace(process_id, mcmc_trace, settings)
+
+
+def _get_initial_state(process_id, settings):
+    rng = np.random.default_rng(process_id)
+    initial_state = np.array(
+        [
+            rng.uniform(
+                low=settings.parameter_ranges[i][0],
+                high=settings.parameter_ranges[i][1],
+            )
+            for i in range(settings.num_parameters)
+        ]
+    )
+    return initial_state
+
+
+def _run_chain(process_id, initial_state, settings):
+    return process_id * initial_state
+
+
+def _save_trace(process_id, mcmc_trace, settings):
+    output_directory = settings.output_directory / pathlib.Path(
+        f"{settings.output_filename}_{process_id}"
+    )
+    os.makedirs(output_directory.parent, exist_ok=settings.overwrite_results)
+    np.save(output_directory, mcmc_trace)
+
+
+# --------------------------------------------------------------------------------------------------
+def main():
+    process_ids = range(num_processes)
+    run_per_process = functools.partial(execute_mtmlda_run, settings=Settings)
+    with multiprocessing.Pool(processes=num_processes) as process_pool:
+        process_pool.map(run_per_process, process_ids)
+
+
+if __name__ == "__main__":
+    main()

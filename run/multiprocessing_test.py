105a9a1fa8dcfd52e4bd96b3dbd26f4bf37111c3
Max <maximilian.kruse@kit.edu>
2024-01-27 14:19:11 +0100

    chore(package structure): Introduce folder structure

--- run/multiprocessing_test.py
+++ run/multiprocessing_test.py
@@ -8,10 +8,11 @@ import numpy as np
 # --------------------------------------------------------------------------------------------------
 num_processes = 2
 
+
 class Settings:
     num_parameters = 4
     parameter_ranges = [[500, 5000], [1e5, 1e6], [5, 50], [5, 50]]
-    output_directory = pathlib.Path("results")
+    output_directory = "results"
     output_filename = "chain"
     overwrite_results = True
 
@@ -25,15 +26,10 @@ def execute_mtmlda_run(process_id, settings):
 
 def _get_initial_state(process_id, settings):
     rng = np.random.default_rng(process_id)
-    initial_state = np.array(
-        [
-            rng.uniform(
-                low=settings.parameter_ranges[i][0],
-                high=settings.parameter_ranges[i][1],
-            )
-            for i in range(settings.num_parameters)
-        ]
-    )
+    initial_state = [
+        rng.uniform(low=settings.parameter_ranges[i][0], high=settings.parameter_ranges[i][1])
+        for i in range(settings.num_parameters)
+    ]
     return initial_state
 
 
@@ -42,7 +38,7 @@ def _run_chain(process_id, initial_state, settings):
 
 
 def _save_trace(process_id, mcmc_trace, settings):
-    output_directory = settings.output_directory / pathlib.Path(
+    output_directory = pathlib.Path(settings.output_directory) / pathlib.Path(
         f"{settings.output_filename}_{process_id}"
     )
     os.makedirs(output_directory.parent, exist_ok=settings.overwrite_results)

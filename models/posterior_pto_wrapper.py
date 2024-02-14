fix(prior): Make prior evaluation more numerically stable

models/posterior_pto_wrapper.py
@@ -5,12 +5,14 @@ import umbridge as ub
 
 
 class UninformLogPrior:
+    is_const = True
+
     def __init__(self, parameter_intervals: np.ndarray, seed: int = 0) -> None:
         self._lower_bounds = parameter_intervals[:, 0]
         self._upper_bounds = parameter_intervals[:, 1]
         self._interval_lengths = self._upper_bounds - self._lower_bounds
-        self._log_prior_const = np.log(1 / np.prod(self._upper_bounds - self._lower_bounds))
         self._rng = np.random.default_rng(seed)
+        self._log_prior_const = -1
 
     def evaluate(self, parameter: np.ndarray) -> float:
         parameter = np.array(parameter[0])
@@ -54,7 +56,8 @@ class LogPosterior:
         if np.isneginf(log_prior):
             log_posterior = -np.inf
         else:
-            log_likelihood = self._log_likelihood.evaluate(parameter, config)
-            log_posterior = log_prior + log_likelihood
+            log_posterior = self._log_likelihood.evaluate(parameter, config)
+            if not self._log_prior.is_const:
+                log_posterior += log_prior
 
         return log_posterior
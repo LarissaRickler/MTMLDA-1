783e5ee1c72e77c071a2a269445c0885a4316e76
Max <maximilian.kruse@kit.edu>
2024-03-16 17:08:41 +0100

    feat(seissol): Add seissol-specific code

---
+++ seissol/sebastian/settings.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import numpy as np
+
+from components import general_settings
+from . import builder
+
+# ==================================================================================================
+parallel_run_settings = general_settings.ParallelRunSettings(
+    num_chains=1,
+    result_directory_path=Path("results_seissol_sebastian"),
+    chain_file_stem=Path("chain"),
+    rng_state_save_file_stem=Path("rng_states"),
+    rng_state_load_file_stem=None,
+    overwrite_results=True,
+)
+
+sampler_setup_settings = general_settings.SamplerSetupSettings(
+    num_levels=2,
+    subsampling_rates=[5, -1],
+    max_tree_height=50,
+    rng_seed_mltree=None,
+    rng_seed_node_init=None,
+    do_printing=True,
+    mltree_path=Path("results_seissol_sebastian") / Path("mltree"),
+    logfile_path=Path("results_seissol_sebastian") / Path("mtmlda.log"),
+    write_mode="w",
+)
+
+sampler_run_settings = general_settings.SamplerRunSettings(
+    num_samples=1000,
+    initial_state=None,
+    num_threads=8,
+    print_interval=100,
+    tree_render_interval=100,
+)
+
+# --------------------------------------------------------------------------------------------------
+inverse_problem_settings = builder.InverseProblemSettings(
+    prior_mean=np.array((5e6,)),
+    prior_covariance=1e12 * np.identity(1),
+    prior_rng_seed=None,
+    ub_model_configs=({"order": 4}, {"order": 5}),
+    ub_model_address="http://localhost:4242",
+    ub_model_name="forward",
+)
+
+sampler_component_settings = builder.SamplerComponentSettings(
+    proposal_step_width=0.1,
+    proposal_covariance=1e12 * np.identity(1),
+    proposal_rng_seed=None,
+    accept_rates_initial_guess=[0.5, 0.7],
+    accept_rates_update_parameter=0.01,
+)
+
+initial_state_settings = builder.InitialStateSettings()
---
+++ seissol/zihua/builder.py
@@ -0,0 +1,122 @@
+import time
+from collections.abc import Callable
+from dataclasses import dataclass
+from functools import partial
+from pathlib import Path
+from typing import Any
+
+import numpy as np
+import umbridge as ub
+
+import src.mtmlda.mcmc as mcmc
+from components import abstract_builder, prior, posterior
+
+
+# ==================================================================================================
+@dataclass
+class InverseProblemSettings(abstract_builder.InverseProblemSettings):
+    prior_intervals: np.ndarray
+    prior_rng_seed: int
+    likelihood_data_dir: Path
+    ub_model_configs: dict[str, str]
+    ub_model_address: str
+    ub_model_name: str
+
+
+@dataclass
+class SamplerComponentSettings(abstract_builder.SamplerComponentSettings):
+    proposal_step_width: float
+    proposal_covariance: np.ndarray
+    proposal_rng_seed: int
+    accept_rates_initial_guess: list[float]
+    accept_rates_update_parameter: float
+
+
+@dataclass
+class InitialStateSettings(abstract_builder.InitialStateSettings):
+    pass
+
+
+# ==================================================================================================
+class ApplicationBuilder(abstract_builder.ApplicationBuilder):
+
+    # ----------------------------------------------------------------------------------------------
+    def __init__(self, process_id: int) -> None:
+        super().__init__(process_id)
+        self._prior_component = None
+
+    # ----------------------------------------------------------------------------------------------
+    def set_up_models(self, inverse_problem_settings: InverseProblemSettings) -> list[Callable]:
+        server_available = False
+        while not server_available:
+            try:
+                if self._process_id == 0:
+                    print("Calling server...")
+                pto_model = ub.HTTPModel(
+                    inverse_problem_settings.ub_model_address,
+                    inverse_problem_settings.ub_model_name,
+                )
+                if self._process_id == 0:
+                    print("Server available\n")
+                server_available = True
+            except:
+                time.sleep(10)
+
+        inverse_problem_settings.rng_seed = self._process_id
+        prior_component = prior.UniformLogPrior(
+            inverse_problem_settings.prior_intervals, inverse_problem_settings.prior_rng_seed
+        )
+        self._prior_component = prior_component
+
+        likelihood_component = self._set_up_likelihood(
+            inverse_problem_settings.likelihood_data_dir, pto_model
+        )
+
+        model_wrapper = posterior.LogPosterior(prior_component, likelihood_component)
+        models = [
+            partial(model_wrapper, config=config)
+            for config in inverse_problem_settings.ub_model_configs
+        ]
+
+        return models
+
+    # ----------------------------------------------------------------------------------------------
+    def set_up_sampler_components(
+        self, sampler_component_settings: SamplerComponentSettings
+    ) -> tuple[Any, Any]:
+        sampler_component_settings.proposal_rng_seed = self._process_id
+        ground_proposal = mcmc.RandomWalkProposal(
+            sampler_component_settings.proposal_step_width,
+            sampler_component_settings.proposal_covariance,
+            sampler_component_settings.proposal_rng_seed,
+        )
+
+        accept_rate_estimator = mcmc.MLAcceptRateEstimator(
+            sampler_component_settings.accept_rates_initial_guess,
+            sampler_component_settings.accept_rates_update_parameter,
+        )
+
+        return ground_proposal, accept_rate_estimator
+
+    # ----------------------------------------------------------------------------------------------
+    def generate_initial_state(self, initial_state_settings: InitialStateSettings) -> np.ndarray:
+        initial_state = self._prior_component.sample()
+        return initial_state
+
+    # ----------------------------------------------------------------------------------------------
+    def _set_up_likelihood(self, data_directory: Path, pto_model: ub.HTTPModel) -> None:
+        space_data, space_variance = np.load(data_directory / Path("space_data.npz")).values()
+        time_data, time_variance = np.load(data_directory / Path("time_data.npz")).values()
+        data = np.concatenate((space_data, time_data))
+
+        space_covariance = np.diag(space_variance)
+        time_covariance = np.diag(time_variance)
+        covariance = np.block(
+            [
+                [space_covariance, np.zeros((space_data.size, time_data.size))],
+                [np.zeros((time_data.size, space_data.size)), time_covariance],
+            ]
+        )
+
+        likelihood_component = posterior.GaussianLLFromPTOMap(pto_model, data, covariance)
+        return likelihood_component

8402b88d2c1c33701ca1e6c248b164f695d99ee7
Zihua Niu <zniu2025@login2.frontera.tacc.utexas.edu>
2024-07-15 11:15:27 -0500

    merge seissol setup with the main branch

--- seissol/zihua_1D/settings.py
+++ seissol/zihua_1D/settings.py
@@ -7,11 +7,11 @@ from . import builder
 
 # ==================================================================================================
 parallel_run_settings = general_settings.ParallelRunSettings(
-    num_chains=4,
-    chain_save_path=Path("results_seissol_zihua_1D/chain"),
-    chain_load_path=None,
-    node_save_path=Path("results_seissol_zihua_1D/final_node"),
-    node_load_path=Path("results_seissol_zihua_1D/final_node"),
+    num_chains=2,
+    chain_save_path=Path("results_T22_seissol_zihua_1D/chain"),
+    chain_load_path=Path("results_T21_seissol_zihua_1D/chain"),
+    node_save_path=Path("results_T22_seissol_zihua_1D/final_node"),
+    node_load_path=None,
     rng_state_save_path=None,
     rng_state_load_path=None,
     overwrite_chain=True,
@@ -20,8 +20,8 @@ parallel_run_settings = general_settings.ParallelRunSettings(
 )
 
 sampler_setup_settings = general_settings.SamplerSetupSettings(
-    num_levels=2,
-    subsampling_rates=[5, -1],
+    num_levels=3,
+    subsampling_rates=[15, 2, -1],
     max_tree_height=50,
     underflow_threshold=-1000,
     rng_seed_mltree=1,
@@ -30,7 +30,7 @@ sampler_setup_settings = general_settings.SamplerSetupSettings(
 )
 
 sampler_run_settings = general_settings.SamplerRunSettings(
-    num_samples=3,
+    num_samples=10,
     initial_state=None,
     num_threads=4,
     print_interval=1,
@@ -39,8 +39,8 @@ sampler_run_settings = general_settings.SamplerRunSettings(
 
 logger_settings = general_settings.LoggerSettings(
     do_printing=True,
-    logfile_path=Path("results_seissol_zihua_1D") / Path("mtmlda"),
-    debugfile_path=Path("results_seissol_zihua_1D") / Path("mtmlda_debug"),
+    logfile_path=Path("results_T22_seissol_zihua_1D") / Path("mtmlda"),
+    debugfile_path=Path("results_T22_seissol_zihua_1D") / Path("mtmlda_debug"),
     write_mode="w",
 )
 
@@ -48,23 +48,23 @@ logger_settings = general_settings.LoggerSettings(
 inverse_problem_settings = builder.InverseProblemSettings(
     prior_intervals=np.array(
         [
-            [1e6, 9e6],
+            [0.5,3.0],
         ]
     ),
     prior_rng_seed=3,
-    ub_model_configs=({"order": 4}, {"order": 5}),
-    ub_model_address="http://localhost:4242",
-    ub_model_name="forward",
-    use_surrogate=False,
+    ub_model_configs=({"meshFile": "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC","order": 3}, {"meshFile": "Ridgecrest_NewModel1_f200_topo1000_noRef_xml_UBC","order": 4}),
+    ub_model_address="http://localhost:4343",
+    ub_model_name="QueuingModel",
+    use_surrogate=True,
     ub_surrogate_address="http://localhost:4243",
     ub_surrogate_name="surrogate",
 )
 
 sampler_component_settings = builder.SamplerComponentSettings(
-    proposal_step_width=0.5,
-    proposal_covariance=1e12 * np.identity(1),
+    proposal_step_width=0.2,
+    proposal_covariance=1.0e1 * np.identity(1),
     proposal_rng_seed=4,
-    accept_rates_initial_guess=[0.5, 0.8],
+    accept_rates_initial_guess=[0.5,0.7,0.8],
     accept_rates_update_parameter=0.01,
 )
 

89259ee6ce3b1bd73a995332d12313cfc4a48a78
Zihua Niu <zniu2025@login2.frontera.tacc.utexas.edu>
2024-10-25 06:29:06 -0500

    add settings for actual run, minor change

--- seissol/zihua_2D/settings.py
+++ seissol/zihua_2D/settings.py
@@ -8,13 +8,16 @@ from . import builder
 
 # ==================================================================================================
 parallel_run_settings = general_settings.ParallelRunSettings(
-    num_chains=3,
-    chain_save_path=Path("results_seissol_zihua_2D/chain"),
-    chain_load_path=None,
-    node_save_path=Path("results_seissol_zihua_2D/final_node"),
-    node_load_path=None,
-    rng_state_save_path=None,
-    rng_state_load_path=None,
+    num_chains=2,
+    chain_save_path=Path("ridgecrest_2D_T28/chain"),
+    # chain_load_path=None,
+    chain_load_path=Path("ridgecrest_2D_T27/chain"),
+    node_save_path=Path("ridgecrest_2D_T28/final_node"),
+    node_load_path=Path("ridgecrest_2D_T27/final_node"),
+    # node_load_path=None,
+    rng_state_save_path=Path("ridgecrest_2D_T28/rng_state"),
+    rng_state_load_path=Path("ridgecrest_2D_T27/rng_state"),
+    # rng_state_load_path=None,
     overwrite_chain=True,
     overwrite_node=True,
     overwrite_rng_states=True,
@@ -22,7 +25,7 @@ parallel_run_settings = general_settings.ParallelRunSettings(
 
 sampler_setup_settings = general_settings.SamplerSetupSettings(
     num_levels=3,
-    subsampling_rates=[15, 2, -1],
+    subsampling_rates=[30, 2, -1],
     max_tree_height=50,
     underflow_threshold=-1e3,
     rng_seed_mltree=1,
@@ -31,17 +34,17 @@ sampler_setup_settings = general_settings.SamplerSetupSettings(
 )
 
 sampler_run_settings = general_settings.SamplerRunSettings(
-    num_samples=2,
+    num_samples=48,
     initial_state=None,
-    num_threads=1,
+    num_threads=4,
     print_interval=1,
     tree_render_interval=1,
 )
 
 logger_settings = general_settings.LoggerSettings(
     do_printing=True,
-    logfile_path=Path("results_seissol_zihua_2D") / Path("mtmlda"),
-    debugfile_path=Path("results_seissol_zihua_2D") / Path("mtmlda_debug"),
+    logfile_path=Path("ridgecrest_2D_T28") / Path("mtmlda"),
+    debugfile_path=Path("ridgecrest_2D_T28") / Path("mtmlda_debug"),
     write_mode="w",
 )
 
@@ -62,7 +65,7 @@ inverse_problem_settings = builder.InverseProblemSettings(
 
 sampler_component_settings = builder.SamplerComponentSettings(
     proposal_step_width=0.2,
-    proposal_covariance=np.diag((np.square(3.0), np.square(3.0))),
+    proposal_covariance=np.diag((np.square(3.0), np.square(1.0))),
     proposal_rng_seed=4,
     accept_rates_initial_guess=[0.5, 0.7, 0.8],
     accept_rates_update_parameter=0.01,
